4f5b9c8e49c11d5a60ceefea5dd9004332ab3bb2
Thibault Le Grand <thibaultlg@gmail.com>
2017-03-06 16:17:35 +0100

    Ajout contrôleur inscription

---
+++ handiadvisor/src/java/fr/sorbonne/m1/controleur/InscriptionControleur.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.sorbonne.m1.controleur;
+
+/**
+ *
+ * @author thibault
+ */
+public class InscriptionControleur {
+    
+}
